86c307f9d9a375d0ae6ae54f819f3f79a1308482
pasha <majid.pashayi443earlier@gmail.com>
2024-09-13 19:27:11 +0330

    process add blog design

--- pages/dashboard/blogs/add.js
+++ pages/dashboard/blogs/add.js
@@ -202,6 +202,8 @@ const Add = ({ onClose, onSuccess, blogToEdit = null }) => {
             />
           </label>
 
+          
+
           <GalleryUpload
             galleryPreview={galleryPreview}
             setGalleryPreview={setGalleryPreview}
@@ -237,7 +239,91 @@ const Add = ({ onClose, onSuccess, blogToEdit = null }) => {
         </div>
       </div>
   
-      <h2 className="text-lg font-semibold mb-4">پیش‌نمایش</h2>
+   {/* پیش‌نمایش */}
+<div className="flex-1 p-4 h-full sm:h-screen border border-gray-300 rounded-lg overflow-y-auto bg-gray-50">
+  {/* محتوا */}
+  {/* اینجا محل جدید محتوای شما قرار می‌گیرد */}
+  <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16 relative">
+    <div className="bg-cover bg-center text-center overflow-hidden"
+      style={{
+        minHeight: "500px",
+        backgroundImage: "url('https://api.time.com/wp-content/uploads/2020/07/never-trumpers-2020-election-01.jpg?quality=85&amp;w=1201&amp;h=676&amp;crop=1')"
+      }}
+      title="Woman holding a mug">
+    </div>
+    <div className="max-w-3xl mx-auto">
+      <div className="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+        <div className="bg-white relative top-0 -mt-32 p-5 sm:p-10">
+          <h1 className="text-gray-900 font-bold text-3xl mb-2">Revenge of the Never Trumpers</h1>
+          <p className="text-gray-700 text-xs mt-2">Written By:
+            <a href="#"
+              className="text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
+              Ahmad Sultani
+            </a> In
+            <a href="#"
+              className="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
+              Election
+            </a>,
+            <a href="#"
+              className="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
+              Politics
+            </a>
+          </p>
+          {/* محتوای مقاله */}
+          <p className="text-base leading-8 my-5">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+          </p>
+          <h3 className3="text-2xl text-center font-bold my-5">#1. What is Lorem Ipsum?</h3>
+
+                <p className3="text-base leading-8 my-5">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+
+                <blockquote className3="border-l-4 text-base italic leading-8 my-5 p-5 text-indigo-600">Lorem Ipsum is simply
+                    dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                    dummy text ever since the 1500s
+                </blockquote>
+
+                <p className3="text-base leading-8 my-5 text-center">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+
+
+                <a href="#"
+                    className3="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
+                    #Election
+                </a>,
+                <a href="#"
+                    className3="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
+                    #people
+                </a>,
+                <a href="#"
+                    className3="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
+                    #Election2020
+                </a>,
+                <a href="#"
+                    className3="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
+                    #trump
+                </a>,<a href="#"
+                    className3="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
+                    #Joe
+                </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
   
       <div className="flex flex-col gap-y-4">
         <div>
