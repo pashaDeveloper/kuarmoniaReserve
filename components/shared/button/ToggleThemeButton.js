6c88ba0c31050dbd4a10f36414f2f81208365493
pasha <majid.pashayi443earlier@gmail.com>
2024-09-25 20:34:20 +0330

    toggle effect

--- components/shared/button/ToggleThemeButton.js
+++ components/shared/button/ToggleThemeButton.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { BsMoonStarsFill, BsSun } from "react-icons/bs";
 import { useTheme } from "@/pages/ThemeContext"; // دریافت تم از کانتکست
 
@@ -7,7 +7,7 @@ const ToggleThemeButton = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
-    setIsDarkMode(document.body.classList.contains('dark'));
+    setIsDarkMode(document.body.classList.contains("dark"));
   }, []);
 
   const handleToggle = () => {
@@ -16,19 +16,18 @@ const ToggleThemeButton = () => {
   };
 
   return (
-<div className="flex justify-center profile-container rounded-full shine-effect">
-<button 
-  onClick={handleToggle} 
-  className="rounded-full w-12 h-12 flex items-center justify-center dark:bg-gray-800 shadow-lg backdrop-blur-lg profile-pic" 
->
-  {isDarkMode ? (
-    <BsSun className="text-white " size={24} />
-  ) : (
-    <BsMoonStarsFill className="text-gray-700" size={24} />
-  )}
-</button>
-
-        </div>
+    <div className="flex justify-center !w-12 !h-12 profile-container rounded-full shine-effect">
+      <button
+        onClick={handleToggle}
+        className="rounded-full !w-12 !h-12 flex items-center justify-center dark:bg-gray-800 shadow-lg backdrop-blur-lg profile-pic"
+      >
+        {isDarkMode ? (
+          <BsSun className="text-white " size={24} />
+        ) : (
+          <BsMoonStarsFill className="text-gray-700" size={24} />
+        )}
+      </button>
+    </div>
   );
 };
 
