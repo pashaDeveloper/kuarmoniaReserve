e6bf778c0dd521419e6ab1ce5cac230d55d91416
pashaDeveloper <majid.pashayi443earlier@gmail.com>
2024-11-01 18:42:32 +0330

    index tag and meta

--- pages/_document.js
+++ pages/_document.js
@@ -2,87 +2,82 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" >
+    <Html lang="en">
       <Head>
-        {/* Primary Meta Tags  */}
+        {/* Primary Meta Tags */}
         <meta
           name="title"
-          content="Travello - Comprehensive travel platform for diverse booking needs"
+          content="مهاجرت و ازدواج در ترکیه و کانادا - راهنمای جامع برای تصمیم‌گیری بهتر"
         />
         <meta
           name="description"
-          content="For all kinds of travel agencies. Including tours, hotel booking, travel experiences, online booking, villa rental, holiday rental, and resort rental"
+          content="با اطلاعات کامل در مورد مهاجرت، ازدواج، و زندگی در ترکیه و کانادا. نکات کلیدی، شرایط قانونی، و خدمات مشاوره برای زوج‌ها و خانواده‌ها."
         />
         <meta
           name="keywords"
-          content="travel, booking, agencies, hotel, resort, rental, holiday, tours, villa, travello template"
+          content="مهاجرت, ازدواج, ترکیه, کانادا, خدمات مشاوره, زندگی, نکات قانونی, زوج‌ها, خانواده‌ها"
         />
+
         <meta name="robots" content="index, follow" />
         <meta content="text/html; charset=utf-8" />
-        <meta name="language" content="perisan" />
+        <meta name="language" content="persian" />
         <meta name="revisit-after" content="7 days" />
         <meta name="author" content="marjan" />
-
-        <meta
-          name="google-site-verification"
-          content="sZPmFgCCG9yDDjVqCsdembcfkOLDihLkjCWdXkxnEVI"
-        />
-        <meta name="msvalidate.01" content="100EE9C6C0FC17AF45101A46F9363D0C" />
 
         {/* Open Graph / Facebook Meta Tags */}
         <meta
           property="og:title"
-          content="Travello - Comprehensive travel agency platform for diverse booking needs worldwide"
+          content="مهاجرت و ازدواج در ترکیه و کانادا"
         />
         <meta
           property="og:site_name"
-          content="Travello - Comprehensive travel agency platform for diverse booking needs worldwide"
+          content="مهاجرت و ازدواج در ترکیه و کانادا"
         />
         <meta
           property="og:url"
-          content="https://travello-template.vercel.app"
+          content="https://your-website-url.com" // URL مربوط به وب‌سایت شما
         />
         <meta
           property="og:description"
-          content="Perfect for all kinds of travel agency. Including tours, hotel booking, activity/event, travel experiences, online booking, room bnb, villa rental, holiday rental, resort rental, cruises, car rentals, real estate, flight ticket, and more"
+          content="راهنمای جامع برای مهاجرت و ازدواج در ترکیه و کانادا با نکات کلیدی و خدمات مشاوره."
         />
         <meta property="og:type" content="website" />
         <meta
           property="og:image"
-          content="https://res.cloudinary.com/dho0rpn5a/image/upload/v1692935077/samples/travello_template.png"
+          content="https://res.cloudinary.com/your-image-url.png" // URL تصویر مربوط به محتوا
         />
 
         {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta
           property="twitter:url"
-          content="https://travello-template.vercel.app"
+          content="https://your-website-url.com" // URL مربوط به وب‌سایت شما
         />
         <meta
           name="twitter:title"
-          content="Travello - Comprehensive travel agency platform for diverse booking needs worldwide"
+          content="مهاجرت و ازدواج در ترکیه و کانادا"
         />
         <meta
           name="twitter:description"
-          content="Perfect for all kinds of travel agency. Including tours, hotel booking, activity/event, travel experiences, online booking, room bnb, villa rental, holiday rental, resort rental, cruises, car rentals, real estate, flight ticket, and more"
+          content="راهنمای جامع برای مهاجرت و ازدواج در ترکیه و کانادا با نکات کلیدی و خدمات مشاوره."
         />
         <meta
           name="twitter:image"
-          content="https://res.cloudinary.com/dho0rpn5a/image/upload/v1692935077/samples/travello_template.png"
+          content="https://res.cloudinary.com/your-image-url.png" // URL تصویر مربوط به محتوا
         />
 
         <meta name="pinterest-rich-pin" content="true" />
         <meta
           name="pinterest:title"
-          content="Travello - Comprehensive travel agency platform for diverse booking needs worldwide"
+          content="مهاجرت و ازدواج در ترکیه و کانادا"
         />
         <meta
           name="pinterest:description"
-          content="Perfect for all kinds of travel agency. Including tours, hotel booking, activity/event, travel experiences, online booking, room bnb, villa rental, holiday rental, resort rental, cruises, car rentals, real estate, flight ticket, and more"
+          content="راهنمای جامع برای مهاجرت و ازدواج در ترکیه و کانادا با نکات کلیدی و خدمات مشاوره."
         />
         <meta
           name="pinterest:image"
-          content="https://res.cloudinary.com/dho0rpn5a/image/upload/v1692935077/samples/travello_template.png"
+          content="https://res.cloudinary.com/your-image-url.png" // URL تصویر مربوط به محتوا
         />
       </Head>
       <body dir="rtl">
