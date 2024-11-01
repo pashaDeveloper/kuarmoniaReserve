e7c1b812d29415690072cf4feb0049fb1340d46c
pashaDeveloper <majid.pashayi443earlier@gmail.com>
2024-11-01 19:18:56 +0330

    add to publish

--- pages/AppWithTracking.js
+++ pages/AppWithTracking.js
@@ -1,10 +1,10 @@
 // // pages/AppWithTracking.js
-// import React from 'react';
+ import React from 'react';
 // import useUserTracking from '@/hooks/useUserTracking';
 
-// const AppWithTracking = ({ children }) => {
+ const AppWithTracking = ({ children }) => {
 //   useUserTracking(); // فراخوانی هوک در داخل Provider
 //   return <>{children}</>;
-// };
+ };
 
-// export default AppWithTracking;
+ export default AppWithTracking;
