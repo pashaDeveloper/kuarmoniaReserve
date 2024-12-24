f21cc59a05acc66f13d6df5551d0ca11beaefc86
pasha <majid.pashayi443earlier@gmail.com>
2024-12-24 15:45:20 +0330

    chnage singup api

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -7,20 +7,17 @@ export const config = {
     externalResolver: true,
   },
 };
+const uploadMiddleware = getUploadMiddleware();
 
 export default function handler(req, res) {
   switch (req.method) {
     case "POST":
       try {
         const upload = getUploadMiddleware("user");
-        upload.single("avatar")(req, res, async (err) => {
-          if (err) {
-            console.error("Upload Error: ", err.message);
-            return res.status(400).json({
-              success: false,
-              message: err.message,
-            });
-          }
+        uploadMiddleware.fields(
+          [{ name: "avatar", maxCount: 1 }],
+          "user" // bucketName
+        )(req, res, async () => {
           try {
             const result = await signUpUser(req);
             res.status(200).json(result);
